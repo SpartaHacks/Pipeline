ce1e77eac32fca4ac32d515f8d50c9f9252eef8f
Your Name <suyash1629@gmail.com>
2017-01-21 17:17:02 -0600

    Automated Pipeline steps to build android application

--- android-pipeline.groovy
+++ android-pipeline.groovy
@@ -1,7 +1,36 @@
 def jenkinsBuild() {
-  node ('android_build') {
-    echo "Hello, world!"
-  }
-}
+    node('android') {
+        try {
+            stage('setup') {
+                checkout scm
+
+                env.ANDROID_HOME = "/root/android_sdk/"
+                env.PATH = env.PATH + ":/root/android_sdk/platform_tools/:/root/android_sdk/tools/"
+            }
+    
+            stage('clean build') {
+                sh "./gradlew clean"
+            }
+    
+            stage('assemble') {
+                sh "./gradlew assembleDebug"
+            }
+    
+            stage('archive app') {
+                step([$class: 'ArtifactArchiver', artifacts: '**/apk/app-debug.apk', fingerprint: true])
+            }
+        } 
+        catch (e) {
+            echo '***Error: Build failed with error ' + e.toString()
+            throw e
+        }
+
+        finally {
+	    echo "Cleaning work directory"
+            sh "git clean -xfd"
+        }
+    }
+}   
 
 return this;
+
