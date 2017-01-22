619d92586764cff4fa4b1901359c75ffc945681d
Your Name <suyash1629@gmail.com>
2017-01-22 12:51:11 -0600

    slack notification feature

--- android-pipeline.groovy
+++ android-pipeline.groovy
@@ -13,11 +13,21 @@ def jenkinsBuild() {
             }
     
             stage('assemble') {
-                sh "./gradlew assembleDebug"
+                if(env.BRANCH_NAME == 'master') {
+                    sh ".gradle assembleRelease"
+                }
+                else {
+                    sh "./gradlew assembleDebug"
+                }
             }
     
             stage('archive app') {
-                step([$class: 'ArtifactArchiver', artifacts: '**/apk/app-debug.apk', fingerprint: true])
+                if(env.BRANCH_NAME == 'master') {
+                    sh ".gradle assembleRelease"
+                }
+                else {
+                    step([$class: 'ArtifactArchiver', artifacts: '**/apk/app-debug.apk', fingerprint: true])
+                }
             }
         } 
         catch (e) {
@@ -28,8 +38,35 @@ def jenkinsBuild() {
         finally {
 	    echo "Cleaning work directory"
             sh "git clean -xfd"
+	    notifyBuild(currentBuild.result)
         }
     }
+}
+
+def notifyBuild(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus =  buildStatus ?: 'SUCCESSFUL'
+
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#FF0000'
+  def subject = "${buildStatus}: Job '${env.JOB_NAME}'"
+  def summary = "${subject} ${env.BUILD_URL}"
+
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#FFFF00'
+  } else if (buildStatus == 'SUCCESSFUL') {
+    color = 'GREEN'
+    colorCode = '#00FF00'
+  } else {
+    color = 'RED'
+    colorCode = '#FF0000'
+  }
+
+  // Send notifications
+  slackSend (color: colorCode, message: summary)
 }   
 
 return this;
